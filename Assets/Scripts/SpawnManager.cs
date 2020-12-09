65342edcdf93446aa57b1e0d1c95dda0faf410b9
raquelfrankel <60672345+raquelfrankel@users.noreply.github.com>
2020-12-09 14:17:33 -0800

    3.1.9 complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLeft : MonoBehaviour
-
+public class SpawnManager : MonoBehaviour
 {
-    private float speed = 10;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        
     }
 }
