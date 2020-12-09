400e0f570a2611e5677c982c5dc2f404a34373dd
raquelfrankel <60672345+raquelfrankel@users.noreply.github.com>
2020-12-09 15:05:49 -0800

    3.2.6

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 
 {
-    private float speed = 10;
+    private float speed = 20;
+    private PlayerController PlayerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (PlayerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
     }
 }
