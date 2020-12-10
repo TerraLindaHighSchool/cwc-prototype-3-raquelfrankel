8740844bc26903bbc1abc6055f0c949f8a81e48b
raquelfrankel <60672345+raquelfrankel@users.noreply.github.com>
2020-12-09 18:52:08 -0800

    3.3.4

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour    
 
 {
-    private float speed = 20;
+    private float speed = 25;
     private PlayerController PlayerControllerScript;
     private float leftBoundary = -15;
     // Start is called before the first frame update
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -22,7 +23,9 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim = GetComponent<Animator>();
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
